b8750c13da51079ca7f317ab46c016436945602e
Jafar Madadi <jafar.madadi76@gmail.com>
2024-06-11 12:54:39 +0430

    User profile image successfully implemented

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -13,24 +13,30 @@ class UserProfileController extends Controller
     function update(Request $request)
     {
         $request->validate([
-            'avatar' => ['nullable', 'image', 'max:1000'],
+            'avatar' => ['nullable', 'image', 'max:500'],
             'name' => ['required', 'string', 'max:50'],
-            'user_id' => ['required', 'string', 'max:50', 'unique:users,user_name'.auth()->user()->id],
-            'email' => ['required', 'email', 'max:50'],
+            'user_id' => ['required', 'string', 'max:50', 'unique:users,user_name,' . auth()->user()->id],
+            'email' => ['required', 'email', 'max:100']
         ]);
 
         $avatarPath = $this->uploadFile($request, 'avatar');
 
         $user = Auth::user();
-        if ($avatarPath) {
-            $user->avatar = $avatarPath;
-        }
+        if ($avatarPath) $user->avatar = $avatarPath;
         $user->name = $request->name;
         $user->user_name = $request->user_id;
         $user->email = $request->email;
+//        if ($request->filled('current_password')) {
+//            $request->validate([
+//                'current_password' => ['required', 'current_password'],
+//                'password' => ['required', 'string', 'min:8', 'confirmed']
+//            ]);
+//            $user->password = bcrypt($request->password);
+//        }
         $user->save();
 
-        // ارسال پاسخ JSON با وضعیت موفقیت
-        return response()->json(['success' => 'Updated Successfully'], 200);
+        notyf()->addSuccess('Updated Successfully.');
+
+        return response(['message' => 'Updated Successfully!'], 200);
     }
 }
--- app/Traits/FileUploadTrait.php
+++ app/Traits/FileUploadTrait.php
@@ -4,20 +4,19 @@ namespace App\Traits;
 
 use Illuminate\Http\Request;
 
-trait FileUploadTrait
-{
-//    function uploadFile(Request $request, string $inputName, ?string $oldPath = null, ?string $path = '/uploads')
-//    {
-//        if ($request->hasFile($inputName)) {
-//
-//            $file = $request->file($inputName);
-//            $fileName = 'Img_' . time() . '_' . uniqid() . '.' . $file->getClientOriginalExtension();
-//
-//            $file->move(public_path($path), $fileName);
-//
-//            return $path.'/'.$fileName;
-//        }
-//
-//        return null;
-//    }
+trait FileUploadTrait {
+
+    function uploadFile(Request $request, string $inputName, ?string $oldPath = null, string $path = '/uploads' ) {
+        if($request->hasFile($inputName)) {
+            $file = $request->{$inputName};
+            $ext = $file->getClientOriginalExtension();
+            $fileName = 'img_'.uniqid().'.'.$ext;
+
+            $file->move(public_path($path), $fileName);
+
+            return $path.'/'.$fileName;
+        }
+
+        return null;
+    }
 }
